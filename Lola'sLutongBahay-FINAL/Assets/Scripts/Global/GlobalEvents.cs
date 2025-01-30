99a3968aba270bf3b8ce92d55d4c8a57bc3754e7
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2025-01-30 15:21:11 +0800

    update global events

---
+++ Lola'sLutongBahay-FINAL/Assets/Scripts/Global/GlobalEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class GlobalEvents
+{
+    public static UnityEvent saveGame;
+    public static UnityEvent saveGameDone;
+    public static UnityEvent saveInventoryDone;
+}
