3bdb7e39cefa682bf611fc90ca99b5084ee0545b
Markdiocre <mark.manuel.business@gmail.com>
2025-06-24 20:26:32 +0800

    Created blueprint of architecture, other files moved to legacy for now

---
+++ Lola'sLutongBahay-FINAL/Assets/Scripts/Utilities/Example.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example 
+{
+    public int AddTwoNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
